1a9d5a2731e37f4bc17356f960ac129b2059fdf6
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-05-29 05:25:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,9 +21,18 @@ def loop_message_n_times(message, limit)
 end
 
 def output_array(array)
-  
+  count = 0
+  while count < array.length do 
+    puts array[count]
+    count += 1
+  end
 end
 
 def return_string_array(array)
-  
+  count = 0
+  while count < array.length do
+    array[coun]t = array[count].to_s
+    count += 1
+  end
+  array
 end
